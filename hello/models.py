89cfad6392477114db3ab8790e68985af9c693f4
felipaugsts <felipaugsts@gmail.com>
2021-03-15 13:43:59 -0300

    2 - adding user

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+
+
+class Jobs(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    Company = models.CharField(max_length=200, null=True)
+    description = models.TextField(max_length=250, null=True)
+    salary = models.IntegerField(null=True, blank=True)
+    Contact = models.CharField(max_length=200, null=True)
+
+
+def __str__(self):
+    return self.title
--- hello/views.py
+++ hello/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Jobs
+
+# Register your models here.
+
+admin.site.register(Jobs)
