8450db77526e88467339ce0a56834fd132ddcb9e
felipaugsts <felipaugsts@gmail.com>
2021-03-15 14:09:13 -0300

    ch(App): Add aplication Files

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -116,11 +116,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 django_heroku.settings(locals())
